adc1379559d5a8796cd94894fa8e910c7b0c40b5
Louis Cho <LouisCho@Ting-Weis-MBP.Home>
2016-05-23 13:32:02 +0800

    Finished skeleton of tests.

--- proj1b/TestArrayDeque1B.java
+++ proj1b/TestArrayDeque1B.java
@@ -1,6 +1,45 @@
 /**
  * Created by LouisCho on 5/23/16.
  */
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestArrayDeque1B {
+
+	@Test
+	public void addFirstTest() {
+
+	}
+
+	@Test
+	public void addLastTest() {
+
+	}
+
+	@Test
+	public void isEqualTest() {
+
+	}
+
+	@Test
+	public void sizeTest() {
+
+	}
+
+	@Test
+	public void removeFirstTest() {
+
+	}
+
+	@Test
+	public void removeLastTest() {
+
+	}
+
+	@Test
+	public void getTest() {
+
+	}
 
 }
--- proj1b/TestLinkedListDeque1B.java
+++ proj1b/TestLinkedListDeque1B.java
@@ -1,5 +1,43 @@
 /**
  * Created by LouisCho on 5/23/16.
  */
+
+import org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestLinkedListDeque1B {
+	@Test
+	public void addFirstTest() {
+
+	}
+
+	@Test
+	public void addLastTest() {
+
+	}
+
+	@Test
+	public void isEqualTest() {
+
+	}
+
+	@Test
+	public void sizeTest() {
+
+	}
+
+	@Test
+	public void removeFirstTest() {
+
+	}
+
+	@Test
+	public void removeLastTest() {
+
+	}
+
+	@Test
+	public void getTest() {
+
+	}
 }
