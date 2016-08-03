bc999e9da6a9979ab02762eb722dfb49c3597487
Louis Cho <LouisCho@Ting-Weis-MacBook-Pro.local>
2016-08-03 09:07:51 +0800

    my version of hw4 finished. not working after puzzle 15. start revising by referring to princeton code.

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,35 +1,30 @@
 package hw4.puzzle;
 
+import java.util.Arrays;
+
 public class Board {
 
 	private int size; // dimension of the board
 	private int[][] tiles;
-	private Board goal;
 
 	public Board(int[][] tiles) {
-		this.tiles = tiles;
 		size = tiles.length;
-		this.goalConstructor();
-	}
+		this.tiles = new int[size][size];
 
-	private void goalConstructor() {
-		int[][] goalTiles = new int[size][size];
-		int currentElement = 1;
-
+		// Need to make a copy of the passed array so the board is immutable.
 		for (int i = 0; i < size; i += 1) {
-			for (int j = 0; j < size; j += 1) {
-				if (currentElement == size * size) {
-					break;
-				}
-				goalTiles[i][j] = currentElement;
-				currentElement += 1;
-			}
+			this.tiles[i] = Arrays.copyOf(tiles[i], size);
 		}
-		goal = new Board(goalTiles);
 	}
 
 	private boolean tileInTheRightPos(int row, int col) {
-		if (tileAt(row, col) != row + size * col + 1) {
+
+		// The right value for the bottom right spot is 0
+		if ((row == size - 1) && (col == size - 1) && (tileAt(row, col) == 0)) {
+			return true;
+		}
+		// Else where if the tile does not have the given value, it is not in the right position.
+		if (tileAt(row, col) != size * row + col + 1) {
 			return false;
 		}
 
@@ -49,7 +44,7 @@ public class Board {
 		int numOfWrongPos = 0;
 		for (int i = 0; i < size; i += 1) {
 			for (int j = 0; j < size; j += 1) {
-				if (!tileInTheRightPos(i, j)) {
+				if ((tileAt(i, j) != 0) && !tileInTheRightPos(i, j)) {
 					numOfWrongPos += 1;
 				}
 			}
@@ -60,11 +55,34 @@ public class Board {
 
 	// We will account for previously made moves later in the solver.
 	public int manhattan() {
+		int manhattanVal = 0;
+		int tile;
+		int tileRow;
+		int tileCol;
 
+		for (int i = 0; i < size; i += 1) {
+			for (int j = 0; j < size; j += 1) {
+				if (tileAt(i, j) != 0) {
+					tile = this.tileAt(i, j);
+					tileRow = (tile - 1) / size;
+					tileCol = tile - tileRow * size - 1;
+					manhattanVal += Math.abs(tileRow - i) + Math.abs(tileCol - j);
+				}
+			}
+		}
+		return manhattanVal;
 	}
 
 	public boolean isGoal() {
-		return (this.equals(goal));
+		for (int i = 0; i < size; i += 1) {
+			for (int j = 0; j < size; j += 1) {
+				if (!tileInTheRightPos(i, j)) {
+					return false;
+				}
+			}
+		}
+
+		return true;
 	}
 
 	public boolean equals(Board y) {
--- hw4/hw4/puzzle/Solver.java
+++ hw4/hw4/puzzle/Solver.java
@@ -1,11 +1,71 @@
 package hw4.puzzle;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.MinPQ;
+import java.util.LinkedList;
 
 public class Solver {
 
-    // DO NOT MODIFY MAIN METHOD
-    /* Uncomment this method once your Solver and Board classes are ready.
+	private class searchNode implements Comparable<searchNode> {
+		private Board board;
+		private int priority;
+
+		public searchNode(Board b, int numOfPreviousMoves) {
+			board = b;
+			priority = b.manhattan() + numOfPreviousMoves;
+		}
+
+		public int priority() {
+			return priority;
+		}
+
+		public int compareTo(searchNode n) {
+			return priority - n.priority();
+		}
+
+		public Board board() {
+			return board;
+		}
+	}
+
+	private int moves;
+	private LinkedList sequenceToSolution = new LinkedList();
+	private MinPQ<searchNode> queue = new MinPQ<>();
+	private searchNode currentNode;
+	private searchNode previousNode;
+
+	public Solver(Board initial) {
+		moves = 0;
+		currentNode = new searchNode(initial, moves);
+		previousNode = new searchNode(initial, moves);
+
+
+		while (!currentNode.board().isGoal()) {
+			queue = new MinPQ<>();
+			for (Board n : BoardUtils.neighbors(currentNode.board())) {
+				if (!n.equals(previousNode.board())) {
+					queue.insert(new searchNode(n, moves));
+				}
+			}
+
+			sequenceToSolution.addLast(currentNode.board());
+			previousNode = currentNode;
+			currentNode = queue.delMin();
+			moves += 1;
+		}
+		// Add solution to the liked list.
+		sequenceToSolution.addLast(currentNode.board());
+	}
+
+	public int moves() {
+		return moves;
+	}
+
+	public Iterable<Board> solution() {
+		return sequenceToSolution;
+	}
+
+
     public static void main(String[] args) {
         In in = new In(args[0]);
         int N = in.readInt();
@@ -21,6 +81,6 @@ public class Solver {
         for (Board board : solver.solution()) {
             StdOut.println(board);
        }
-    }*/
+    }
 
 }
